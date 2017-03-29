d635e75c78e59f8153a3c07185cbab901408ac7a
bigrice1018 <bigrice1018@gmail.com>
2017-03-29 20:02:44 +0800

    Ore Gen Fix

--- settings.bat
+++ settings.bat
@@ -8,4 +8,4 @@ set FORGEJAR=AWTS2-1.10.2-12.18.3.2254-universal.jar
 rem can be changed by user
 set MIN_RAM=512M
 set MAX_RAM=4098M
-set JAVA_PARAMETERS=-XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10
+set JAVA_PARAMETERS=-XX:+AggressiveOpts -XX:+UseLargePages -XX:+UseG1GC -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=10 -XX:SoftRefLRUPolicyMSPerMB=10000 -XX:ParallelGCThreads=3
